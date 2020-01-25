ecaf3c488f48b9b81da3e24b70896ccb1f7546d3
chinmay6345 <chinmay8116@gmail.com>
2020-01-25 23:21:18 +0530

    commit

---
+++ Alternating Characters/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Alternating_Characters
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            String input = "beabeefeab";
+            char[] array = input.ToCharArray();
+
+            int count = 0;
+            for (int i = 1, j = 0; i < input.Length; i++, j++)
+            {
+
+                if(array[i]==array[j])
+                {
+                    count++;
+                }
+            }
+                Console.WriteLine(count);
+            Console.ReadLine();
+        }
+    }
+}
