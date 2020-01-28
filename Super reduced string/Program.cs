371bd840e2435be630b4675dc12b049a619bc12b
chinmay6345 <chinmay8116@gmail.com>
2020-01-28 08:53:49 +0530

    commit

---
+++ Super reduced string/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+//https://www.hackerrank.com/challenges/reduced-string/problem
+
+namespace Super_reduced_string
+{
+    public class Program
+    {
+        public static void Main(String[] args)
+        {
+            String inputstring = Console.ReadLine();
+            for (int i = 1; i < inputstring.Length; i++)
+            {
+                char ith = inputstring[i];
+                char ithplusonlst = inputstring[i - 1];
+                if (ith.Equals(ithplusonlst))
+                {
+                    inputstring = inputstring.Substring(0, i - 1) + inputstring.Substring(i + 1);
+                    i = 0;
+                }
+            }
+            Console.WriteLine(inputstring.Length == 0 ? "Empty String" : inputstring);
+        }
+    }
+}
---
+++ Super reduced string/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("Super reduced string")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Super reduced string")]
+[assembly: AssemblyCopyright("Copyright ©  2020")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("2c7d5caf-92ea-4478-a955-e7bde4007670")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
