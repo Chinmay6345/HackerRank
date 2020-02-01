b9f0de6d6cec71424750229894a273bd535bca08
chinmay6345 <chinmay8116@gmail.com>
2020-02-01 14:21:09 +0530

    commit

---
+++ Left rotation/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Left_rotation
+{
+    public class AppHelper
+    {
+        public static void LeftRotation(Int32[] arr, Int32 d)
+        {
+            List<Int32> lst = arr.ToList<Int32>();
+            for(int i=0;i<d;i++)
+            {
+                int k = lst[0];
+                lst.RemoveAt(0);
+                lst.Add(k);
+            }
+             Console.WriteLine(String.Join(" ", lst.Select(a => a)));
+        }
+    }
+
+
+    public class Program
+    {
+        public static void Main(String[] args)
+        {
+            String[] ip = Console.ReadLine()
+                         .Split(new Char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries);
+            Int32[] array = Console.ReadLine()
+                            .Split(new Char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries)
+                            .Select(a => Convert.ToInt32(a))
+                            .ToArray<Int32>();
+            AppHelper.LeftRotation(array, Convert.ToInt32(ip[1]));
+            Console.ReadLine();
+        }
+    }
+
+}
