174725ac3b056bea111db940a99562bc97d3158d
chinmay6345 <chinmay8116@gmail.com>
2020-02-03 07:24:38 +0530

    commit

---
+++ Migratory Birds/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+//https://www.hackerrank.com/challenges/migratory-birds/problem
+
+namespace Migratory_Birds
+{
+    public class Program
+    {
+        public static void Main(String[] args)
+        {
+            Int32 n = int.Parse(Console.ReadLine());
+            Int32[] array = Console.ReadLine().Split(' ').Select(a => int.Parse(a)).ToArray();
+            Int32 max = 0;
+            List<Int32> lstIntegers = new List<Int32>();
+            var p = array.GroupBy(a => a).Where(a => a.Count<int>() > 1).Select(a => new { key = a.Key, Count = a.Count() }).OrderByDescending(a => a.Count);
+            foreach (var item in p)
+            {
+                if (item.Count >= max)
+                {
+                    lstIntegers.Add(item.key);
+                    max = item.Count;
+                }
+            }
+            if (lstIntegers.Count > 1)
+            {
+                Console.WriteLine(lstIntegers.Min());
+            }
+            else
+            {
+                Console.WriteLine(lstIntegers[0]);
+            }
+        }
+    }
+
+}
