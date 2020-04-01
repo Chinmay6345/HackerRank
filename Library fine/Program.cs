8a7603ba99728d02a094b4cc2471cfcc3e42495f
chinmay6345 <chinmay8116@gmail.com>
2020-04-01 09:40:45 +0530

    added description library fine

--- Library fine/Program.cs
+++ Library fine/Program.cs
@@ -4,6 +4,53 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 //https://www.hackerrank.com/challenges/library-fine/problem
+/*
+Your local library needs your help! Given the expected and actual return dates for a library book, create a program that calculates the fine (if any). The fee structure is as follows:
+
+If the book is returned on or before the expected return date, no fine will be charged (i.e.: .
+If the book is returned after the expected return day but still within the same calendar month and year as the expected return date, .
+If the book is returned after the expected return month but still within the same calendar year as the expected return date, the .
+If the book is returned after the calendar year in which it was expected, there is a fixed fine of .
+Charges are based only on the least precise measure of lateness. For example, whether a book is due January 1, 2017 or December 31, 2017, if it is returned January 1, 2018, that is a year late and the fine would be .
+
+Function Description
+
+Complete the libraryFine function in the editor below. It must return an integer representing the fine due.
+
+libraryFine has the following parameter(s):
+
+d1, m1, y1: returned date day, month and year
+d2, m2, y2: due date day, month and year
+Input Format
+
+The first line contains  space-separated integers, , denoting the respective , , and  on which the book was returned.
+The second line contains  space-separated integers, , denoting the respective , , and  on which the book was due to be returned.
+
+Constraints
+
+Output Format
+
+Print a single integer denoting the library fine for the book received as input.
+
+Sample Input
+
+9 6 2015
+6 6 2015
+Sample Output
+
+45
+Explanation
+
+Given the following dates:
+Returned: 
+Due: 
+
+Because , we know it is less than a year late.
+Because , we know it's less than a month late.
+Because , we know that it was returned late (but still within the same month and year).
+
+Per the library's fee structure, we know that our fine will be . We then print the result of  as our output. 
+*/
 namespace Library_fine
 {
     public static class AppHelper
