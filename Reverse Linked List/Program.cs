50797f7f8d0e9f97a938c3feb9a62a3869418824
chinmay6345 <chinmay8116@gmail.com>
2020-04-11 12:08:51 +0530

    commit-reverse singly linked list

---
+++ Reverse Linked List/Program.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Reverse_Linked_List
+{
+    // Complete the reverse function below.
+
+
+    public class ListNode
+    {
+        public int val;
+        public ListNode next;
+        public ListNode(int x) { val = x; }
+    }
+    public class AppHelper
+    {
+        public Int32 GetCountOfElements(ListNode head)
+        {
+            ListNode p = head;
+            Int32 count = 0;
+            while (p != null)
+            {
+                count = count + 1;
+                p = p.next;
+            }
+            return count;
+        }
+        public ListNode ReversedLinkedList(ListNode head)
+        {
+            if (head == null || head.next == null)
+            {
+                return head;
+            }
+            Int32 count = GetCountOfElements(head);
+            Int32[] arr = new Int32[count];
+            Int32 i = 0;
+            ListNode p = head;
+            while (p != null)
+            {
+                arr[i] = p.val;
+                i = i + 1;
+                p = p.next;
+            }
+            Array.Reverse(arr);
+            i = 0;
+            p = head;
+            while (p != null)
+            {
+                p.val = arr[i];
+                p = p.next;
+                i = i + 1;
+            }
+            return head;
+        }
+    }
+
+    public class Solution
+    {
+        public ListNode ReverseList(ListNode head)
+        {
+            AppHelper helper = new AppHelper();
+            return helper.ReversedLinkedList(head);
+        }
+    }
+}
