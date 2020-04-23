0ce85e9022e983b7b4e14ad3a3934e08e05713a8
Chinmay <Chinmay8116@gmail.com>
2020-04-23 17:44:47 +0530

    commit-doubly linked list(reverse)

---
+++ Reverse Doubly Linked List/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+//https://www.hackerrank.com/challenges/reverse-a-doubly-linked-list/problem
+namespace Reverse_Doubly_Linked_List
+{
+
+
+    public class DoublyLinkedListNode
+    {
+        int data;
+        public DoublyLinkedListNode next;
+        public DoublyLinkedListNode prev;
+    }
+
+
+    class Program
+    {
+        static DoublyLinkedListNode reverse(DoublyLinkedListNode head)
+        {
+            DoublyLinkedListNode temp = null;
+            DoublyLinkedListNode p = head;
+            if (p == null)
+            {
+                return null;
+            }
+            else if (p.next == null)
+            {
+                p = head;
+                return head;
+            }
+            while (p != null)
+            {
+                temp = p.prev;
+                p.prev = p.next;
+                p.next = temp;
+                p = p.prev;
+            }
+            if (temp != null)
+            {
+                temp = temp.prev;
+            }
+            return temp;
+
+        }
+
+        static void Main(string[] args)
+        {
+        }
+    }
+}
