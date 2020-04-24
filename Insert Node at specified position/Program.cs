7fda1849f8e3adee062e4813f7d99ec31a903173
Chinmay <Chinmay8116@gmail.com>
2020-04-24 19:21:19 +0530

    commit-insert node at specified position Singly

---
+++ Insert Node at specified position/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+//https://www.hackerrank.com/challenges/insert-a-node-at-a-specific-position-in-a-linked-list/problem
+
+namespace Insert_Node_at_specified_position
+{
+    class Program
+    {
+        public class SinglyLinkedListNode
+        {
+            public int data;
+            public SinglyLinkedListNode next;
+        }
+        public static SinglyLinkedListNode insertNodeAtPosition(SinglyLinkedListNode head,
+                                                     int data, int position)
+        {
+            SinglyLinkedListNode p = head;
+            SinglyLinkedListNode newnode = new SinglyLinkedListNode();
+            if (position == 0)
+            {
+                newnode.next = head;
+                head = newnode;
+                return head;
+            }
+            else
+            {
+                for (Int32 i = 0; i < position - 1; i++)
+                {
+                    p = p.next;
+                }
+                newnode.next = p.next;
+                p.next = newnode;
+                return head;
+            }
+        }
+        static void Main(string[] args)
+        {
+        }
+    }
+}
