19eb3ef65b994603f9501a40dc4af20c4c082e00
Chinmay <Chinmay8116@gmail.com>
2020-04-24 19:33:18 +0530

    commit-add node at head singly linked list

---
+++ Insert node at head of Singly Linked List/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+//https://www.hackerrank.com/challenges/insert-a-node-at-the-head-of-a-linked-list/problem
+namespace Insert_node_at_head_of_Singly_Linked_List
+{
+    public class SinglyLinkedListNode
+    {
+        public int data;
+        public SinglyLinkedListNode next;
+    }
+    class Program
+    {
+
+        public static SinglyLinkedListNode insertNodeAtPosition(SinglyLinkedListNode head)
+        {
+            SinglyLinkedListNode newnode = new SinglyLinkedListNode();
+            newnode.next = head;
+            head = newnode;
+            return head;
+        }
+        static void Main(string[] args)
+        {
+        }
+    }
+
+}
+
