50d3e7a17ca7840997590f41b87f4aed7d1612a8
Chinmay <Chinmay8116@gmail.com>
2020-07-19 10:51:21 +0530

    commit-d pairs

---
+++ Divisible Pairs/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Linq;
+
+//https://www.hackerrank.com/challenges/divisible-sum-pairs/problem
+
+namespace Divisible_Pairs
+{
+    public static class AppHelper
+    {
+        public static void Pairs(Int32 k, Int32[] arr)
+        {
+            try
+            {
+                Int32[] pairsArr = new Int32[k];
+                Int32 count = 0;
+                foreach (Int32 value in arr)
+                {
+                    Int32 mod = value % k;
+                    count += pairsArr[(k - mod) % k];
+                    pairsArr[mod]++;
+
+                }
+                Console.WriteLine(count);
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+        }
+    }
+
+    public class Program
+    {
+        public static void Main(String[] args)
+        {
+            Int32[] arr = Console.ReadLine()
+                            .Split(new Char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries)
+                            .Select(g => Convert.ToInt32(g))
+                            .ToArray<Int32>();
+            Int32[] arr1 = Console.ReadLine()
+                            .Split(new Char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries)
+                            .Select(g => Convert.ToInt32(g))
+                            .ToArray<Int32>();
+            AppHelper.Pairs(arr[1], arr1);
+        }
+    }
+
+}
