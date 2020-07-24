9036ef417c1c325ad1daa5bf48dd517bdb21963d
Chinmay <Chinmay8116@gmail.com>
2020-07-24 11:11:46 +0530

    added code for left rotate

--- Left rotation/Program.cs
+++ Left rotation/Program.cs
@@ -21,16 +21,18 @@ namespace Left_rotation
             }
             Console.WriteLine(String.Join(" ", lst.Select(a => a)));
         }
-        public static int Push<T>(this T[] source, T value)
+        public static void LeftRotateArray(Int32[] arr, Int32 n, Int32 d)
         {
-            var index = Array.IndexOf(source, default(T));
-
-            if (index != -1)
+            Int32[] nums = new Int32[n];
+            for (int i = 0; i < n; i++)
             {
-                source[index] = value;
+                nums[(i + (n - d)) % n] = arr[i];
             }
-
-            return index;
+            for (int i = 0; i < n; i++)
+            {
+                arr[i] = nums[i];
+            }
+            Console.WriteLine(String.Join(" ", arr.Select(g => g)));
         }
     }
 
@@ -39,16 +41,13 @@ namespace Left_rotation
     {
         public static void Main(String[] args)
         {
-            //String[] ip = Console.ReadLine()
-            //             .Split(new Char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries);
-            //Int32[] array = Console.ReadLine()
-            //                .Split(new Char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries)
-            //                .Select(a => Convert.ToInt32(a))
-            //                .ToArray<Int32>();
-            //AppHelper.LeftRotation(array, Convert.ToInt32(ip[1]));
-            int[] myArray = { 1, 2, 3 };
-            Array.Resize(ref myArray, myArray.Length + 1);
-myArray[myArray.GetUpperBound(0)] = 5;
+            String[] ip = Console.ReadLine()
+                         .Split(new Char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries);
+            Int32[] array = Console.ReadLine()
+                            .Split(new Char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries)
+                            .Select(a => Convert.ToInt32(a))
+                            .ToArray<Int32>();
+            AppHelper.LeftRotation(array, Convert.ToInt32(ip[1]));
             Console.ReadLine();
         }
     }
